5fd364949b194338890007ceb14b4c7a67f04a6d
M2U7BF <yhvh4321@gmail.com>
2022-12-10 19:13:50 +0900

    fix : 不要なオプションを削除

--- website/models.py
+++ website/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Emotion(models.Model):
     emotion_name = models.CharField(max_length=50)
     emotion_type = models.CharField(max_length=50)
-    emotion_point = models.SmallIntegerField(max_length=2)
+    emotion_point = models.SmallIntegerField()
 
 class EmotionRecords(models.Model):
     emotion_id = models.ForeignKey(Emotion, on_delete=models.CASCADE)
