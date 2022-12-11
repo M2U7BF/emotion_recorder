232b080c7a181dd1126e664e5d9083e2a3c61517
M2U7BF <yhvh4321@gmail.com>
2022-12-11 18:18:03 +0900

    upgrade : 分析画面のリンクを追加

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, timeline
+from .views import index, timeline, analysis
 
 urlpatterns = [
     path("", index, name="index"),
     path("timeline", timeline, name="index"),
+    path("analysis", analysis, name="analysis"),
 ]
